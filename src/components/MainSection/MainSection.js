69733c7ee7fe008c96767da124ddf5cc5b6709f9
Swastik <swastik.sonkusare18@gmail.com>
2021-07-15 11:38:24 +0530

    added: create-todos, delete-todos and states

--- src/components/MainSection/MainSection.js
+++ src/components/MainSection/MainSection.js
@@ -1,14 +1,82 @@
-import React from "react";
+import React, { useRef, useState, useEffect } from "react";
+
+import { v4 as uuidv4 } from "uuid";
 
 import moonLogo from "../../assets/images/icon-moon.svg";
 import sunLogo from "../../assets/images/icon-sun.svg";
 import checkIcon from "../../assets/images/icon-check.svg";
 import crossIcon from "../../assets/images/icon-cross.svg";
 
+import { data } from "../../data";
+
 import "./MainSection.scss";
 
 const MainSection = () => {
-  const todoItems = ["hello", "goodbye", "hi", "bye"];
+  const [todoItems, setTodoItems] = useState([]);
+  const [filteredTodos, setFilteredTodos] = useState([]);
+  const [status, setStatus] = useState();
+  const [userInput, setUserInput] = useState("");
+
+  const categories = ["All", "Active", "Completed"];
+
+  const todoItemRef = useRef();
+
+  const createTodoHandler = (userInput, e) => {
+    e.preventDefault();
+
+    if (!userInput) return;
+
+    addTask(userInput);
+    setUserInput("");
+  };
+
+  const addTask = (userInput) => {
+    let copy = [...todoItems];
+    copy = [...copy, { id: uuidv4(), task: userInput, complete: false }];
+
+    setTodoItems(copy);
+  };
+
+  const taskCompletedHandler = (id) => {
+    let mapped = todoItems.map((task) => {
+      return task.id === id
+        ? { ...task, complete: !task.complete }
+        : { ...task };
+    });
+
+    setTodoItems(mapped);
+  };
+
+
+  const filterHandler = () => {
+    switch(status) {
+      case "Completed":
+        setFilteredTodos(todoItems.filter(todoItem => todoItem.complete === true))
+        break;
+
+        case "Active":
+          setFilteredTodos(todoItems.filter(todoItem => todoItem.complete === false))
+          break;
+
+        default:
+          setFilteredTodos(todoItems) 
+    }
+  }
+
+
+  const statusHandler = (category) => {
+    setStatus(category)
+    
+  }
+
+  const clearCompletedTodosHandler = () => {
+    setTodoItems(todoItems.filter(item => item.complete !== true))
+  }
+
+  useEffect(() => {
+
+    filterHandler();
+  }, [todoItems, status])
 
   return (
     <main>
@@ -18,35 +86,49 @@ const MainSection = () => {
           <img className="todo__theme" src={moonLogo} alt={moonLogo}></img>
         </div>
 
-        <div className="todo-create">
+        <form
+          className="todo-create"
+          onSubmit={(e) => createTodoHandler(userInput, e)}
+        >
           <span></span>
           <input
+            type="text"
             className="todo-create__input"
             placeholder="Create a new todo..."
+            value={userInput}
+            onChange={(e) => setUserInput(e.target.value)}
           ></input>
-        </div>
+        </form>
 
         <div className="todo__container">
-          <ul className="todo__list">
-            {todoItems.map((item) => (
-              <>
-                <li className="todo__item">
-                  <span></span>
-                  <small>{item}</small>
-                </li>
-              </>
-            ))}
-          </ul>
+          <div className="todo__list">
+            {filteredTodos.length
+              ? filteredTodos.map((item) => (
+                  <>
+                    <div
+                      className={
+                        item.complete ? "todo__item complete" : "todo__item"
+                      }
+                      onClick={() => taskCompletedHandler(item.id)}
+                      ref={todoItemRef}
+                    >
+                      <span></span>
+                      <small>{item.task}</small>
+                    </div>
+                  </>
+                ))
+              : "No items left"}
+          </div>
 
           <div className="todo__status">
-            <div className="todo__left">5 items left</div>
+            <div className="todo__left">{todoItems.length} items left</div>
             <div className="todo__status-content">
-              <span>All</span>
-              <span>Active</span>
-              <span>Completed</span>
+              {categories.map((category) => (
+                <span onClick={() =>statusHandler(category)}>{category}</span>
+              ))}
             </div>
 
-            <div className="todo__completed">Clear Completed</div>
+            <div className="todo__completed" onClick={clearCompletedTodosHandler}>Clear Completed</div>
           </div>
         </div>
       </div>
---
+++ src/data.js
@@ -0,0 +1,12 @@
+export const data = [
+  {
+    id: 1,
+    task: "Give dog a bath",
+    complete: false,
+  },
+  {
+    id: 2,
+    task: "Do laundry",
+    complete: false,
+  },
+];
