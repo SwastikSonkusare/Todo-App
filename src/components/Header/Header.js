f344a23a9182918e5d096df29e08e0d755889fa6
Swastik <swastik.sonkusare18@gmail.com>
2021-07-16 18:17:24 +0530

    added: dark and light mode

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,11 @@
 import React from 'react'
 
 import styled from "styled-components";
-import bg1 from "../../assets/images/bg-desktop-dark.jpg";
-import bg2 from "../../assets/images/bg-desktop-light.jpg";
+import bg1 from "../../assets/images/bg-desktop-light.jpg";
+import bg2 from "../../assets/images/bg-desktop-dark.jpg";
+
+import smallBg1 from '../../assets/images/bg-mobile-light.jpg';
+import smallBg2 from '../../assets/images/bg-mobile-dark.jpg';
 
 const Header = ({ lightTheme }) => {
 
@@ -11,7 +14,13 @@ const Header = ({ lightTheme }) => {
         background-image: url(${lightTheme ? bg1 : bg2});
         background-repeat: no-repeat;
         background-size: cover;
+        background-position: center center;
         height: 300px;
+        
+        @media only screen and (max-width: 31.25em) {
+            background-image: url(${lightTheme ? smallBg1 : smallBg2});
+            background-position: unset;
+        }
     `;
 
     return (
